efaeadf8b4237f198a7c5b48b7039db263cc0e60
oolongmilkteax <rachelllhooo@gmail.com>
2021-03-29 15:12:59 +0800

    Correct spelling in about page

--- src/contents/About.js
+++ src/contents/About.js
@@ -12,9 +12,9 @@ class About extends Component {
                 People often ask me why I chose to major in business analytics.<br></br>
                 To be honest, business administration was what I thought I'll end up majoring in.
                 However that all changed when I joined Cruger Haus SG Pte Ltd, a market research company.
-                I was shocked to find out how much our clients were willing to spend on collecting data and it made me wonder what they were going to do with them and how it would be useful.
-                I started doing some research on my own and it took my down the rabbit hole of analytics.                
-                I realised that more companies were starting to embrace big data and analytics, which meant an inrease in demand for such skillsets.
+                I was shocked to find out how much our clients were willing to spend on collecting data and it made me wonder what they were going to do with it and how it would be useful.
+                I started doing some research on my own and it took me down the rabbit hole of analytics.                
+                I realised that more companies were starting to embrace big data and analytics, which meant an increase in demand for such skillsets.
                 But what intrigued me the most was that through these statistics and figures, people are able to draw trends, spot anomalies and use them to come up with new solutions or streamline operations to further improve the organisation.
                 This resonates strongly with my personal belief that 'change is the only constant' in the fast-paced world that we live in today, and that is why I decided to major in business analytics. 
                 </p>
